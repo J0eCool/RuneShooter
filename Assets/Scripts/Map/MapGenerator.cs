1033c9c5be683997e6827016cf1be040bfd1e891
J0eCool <count.j0ecool@gmail.com>
2016-04-02 17:58:50 -0700

    Generate all walls on game start

---
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+class Room {
+	public bool doorToEast = false;
+	public bool doorToNorth = false;
+}
+
+public class MapGenerator : JComponent {
+	[SerializeField] private GameObject roomPrefab;
+	[SerializeField] private GameObject wallPrefab;
+	[SerializeField] private float roomWidth = 14.333f;
+	[SerializeField] private float roomHeight = 10.0f;
+
+	protected override void OnStart() {
+		int width = 1;
+		int height = 2;
+		Room[,] rooms = new Room[width, height];
+		for (int i = 0; i < width; ++i) {
+			for (int j = 0; j < height; ++j) {
+				rooms[i, j] = new Room();
+				GameObject room = Instantiate(roomPrefab);
+				room.transform.position = positionForRoom(i, j);
+			}
+		}
+		rooms[0, 0].doorToNorth = true;
+
+		for (int i = 0; i < width; ++i) {
+			for (int j = -1; j < height; ++j) {
+				Room cur = null;
+				Room next = null;
+				if (j >= 0) {
+					cur = rooms[i, j];
+				}
+				if (j + 1 < height) {
+					next = rooms[i, j + 1];
+				}
+
+				bool shouldSpawnWall = (cur != null) || (next != null);
+				if (shouldSpawnWall) {
+					bool wallHasDoor = (cur != null) && cur.doorToNorth;
+					GameObject wall = Instantiate(wallPrefab);
+					wall.transform.position = positionForRoom(i, j + 0.5f);
+					RandomlySizedDoor door = wall.GetComponent<RandomlySizedDoor>();
+					door.Init(totalWallSize: roomWidth, isVertical: false, isDoor: wallHasDoor);
+				}
+			}
+		}
+
+		for (int j = 0; j < height; ++j) {
+			for (int i = -1; i < width; ++i) {
+				Room cur = null;
+				Room next = null;
+				if (i >= 0) {
+					cur = rooms[i, j];
+				}
+				if (i + 1 < width) {
+					next = rooms[i + 1, j];
+				}
+
+				bool shouldSpawnWall = (cur != null) || (next != null);
+				if (shouldSpawnWall) {
+					bool wallHasDoor = (cur != null) && cur.doorToEast;
+					GameObject wall = Instantiate(wallPrefab);
+					wall.transform.position = positionForRoom(i + 0.5f, j);
+					RandomlySizedDoor door = wall.GetComponent<RandomlySizedDoor>();
+					door.Init(totalWallSize: roomHeight, isVertical: true, isDoor: wallHasDoor);
+				}
+			}
+		}
+	}
+
+	private Vector3 positionForRoom(float i, float j) {
+		return transform.position + new Vector3(i * roomWidth, j * roomHeight);
+	}
+}
--- Assets/Scripts/Map/RandomlySizedDoor.cs
+++ Assets/Scripts/Map/RandomlySizedDoor.cs
@@ -4,14 +4,16 @@ using System.Collections.Generic;
 
 public class RandomlySizedDoor : JComponent {
 	[SerializeField] private GameObject wallPrefab;
-	[SerializeField] private float totalWallSize;
-	[SerializeField] private bool isVertical = false;
-	[SerializeField] private bool isDoor = true;
 	[SerializeField] private float minWallSize = 1.0f;
 	[SerializeField] private float minDoorSize = 2.0f;
 	[SerializeField] private float maxDoorSize = 3.5f;
 
-	protected override void OnStart() {
+	public void Init(float totalWallSize, bool isVertical, bool isDoor) {
+		if (!isDoor) {
+			spawnWall(isVertical, 0.0f, totalWallSize);
+			return;
+		}
+
 		float doorWidth = normalizedRandom(minDoorSize, maxDoorSize);
 		float halfTotalSize = totalWallSize / 2.0f;
 		float halfDoorSize = doorWidth / 2.0f;
@@ -21,12 +23,12 @@ public class RandomlySizedDoor : JComponent {
 		float doorLeft = doorCenter - halfDoorSize;
 		float leftSize = halfTotalSize + doorLeft;
 		float leftPos = leftSize / 2.0f - halfTotalSize;
-		spawnWall(leftPos, leftSize);
+		spawnWall(isVertical, leftPos, leftSize);
 
 		float doorRight = doorCenter + halfDoorSize;
 		float rightSize = halfTotalSize - doorRight;
 		float rightPos = halfTotalSize - rightSize / 2.0f;
-		spawnWall(rightPos, rightSize);
+		spawnWall(isVertical, rightPos, rightSize);
 	}
 
 	private float normalizedRandom(float lo, float hi) {
@@ -35,16 +37,21 @@ public class RandomlySizedDoor : JComponent {
 		return Random.Range(lo + randDelta, hi - randDelta);
 	}
 
-	private void spawnWall(float offset, float size) {
+	private void spawnWall(bool isVertical, float offset, float size) {
 		GameObject wall = GameObject.Instantiate(wallPrefab);
 		wall.transform.parent = transform;
 
 		Vector3 scale = wall.transform.localScale;
-		scale.x = size;
-		wall.transform.localScale = scale;
-
 		Vector3 pos = transform.position;
-		pos.x += offset;
+		if (isVertical) {
+			scale.y = size;
+			pos.y += offset;
+		} else {
+			scale.x = size;
+			pos.x += offset;
+		}
+		wall.transform.localScale = scale;
 		wall.transform.position = pos;
+
 	}
 }
