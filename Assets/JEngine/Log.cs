a022c9fe33d5aa25c8e3ae039907b1ad7c7996e4
J0eCool <count.j0ecool@gmail.com>
2016-04-03 17:49:47 -0700

    Add formatted log helper

---
+++ Assets/JEngine/Log.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Log {
+	public static void Format(string formatString, params object[] args) {
+		Debug.Log(string.Format(formatString, args));
+	}
+}
