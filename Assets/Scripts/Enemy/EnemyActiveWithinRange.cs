4ff1169baac35abee4f6427aedb710040068d7f9
J0eCool <count.j0ecool@gmail.com>
2016-05-01 00:07:46 -0700

    Enemies move

---
+++ Assets/Scripts/Enemy/EnemyActiveWithinRange.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyMovement : JComponent { }
+
+public class EnemyActiveWithinRange : JComponent {
+	[SerializeField] private float range = 10.0f;
+
+	[StartComponent] private EnemyMovement movement;
+	[StartComponent] new private Rigidbody2D rigidbody;
+	private Transform player;
+
+	protected override void OnStart() {
+		player = PlayerManager.Instance.Player.transform;
+	}
+
+	protected override void OnUpdate() {
+		Vector3 playerPos = player.position;
+		Vector3 delta = transform.position - playerPos;
+		bool inRange = delta.sqrMagnitude < range * range;
+		movement.enabled = inRange;
+		if (!inRange) {
+			rigidbody.velocity = Vector2.zero;
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy/EnemyMoveTowardPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyMoveTowardPlayer : EnemyMovement {
+	[SerializeField] private float speed = 1.5f;
+	[SerializeField] private float range = 2.0f;
+
+	[StartComponent] new private Rigidbody2D rigidbody;
+	private Transform player;
+
+	protected override void OnStart() {
+		player = PlayerManager.Instance.Player.transform;
+	}
+
+	protected override void OnFixedUpdate() {
+		Vector2 delta = player.position - transform.position;
+		Vector2 vel = Vector2.zero;
+		if (delta.sqrMagnitude > range * range) {
+			Vector2 dir = delta.normalized;
+			vel = dir * speed;
+		}
+		rigidbody.velocity = vel;
+	}
+}
